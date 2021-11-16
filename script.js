22b65a20e413e78573b3803388db8de7504e7017
Sally E <sme418@nyu.edu>
2021-11-16 00:00:43 -0500

    added JSON & image folder

---
+++ script.js
@@ -0,0 +1,69 @@
+let contentGridElement = document.getElementByID("content");
+
+let jsonDatabase = [
+
+  {
+    "name": "Lora",
+    "picture_url": "images/Lora.png",
+    "favorite_color": "#5C374C",
+    'crafting_specialty': "Charm-making",
+    "favorite_food": "Ruby-Stew Buloofo"
+  },
+  {
+    "name": "Jin",
+    "picture_url": "images/Jin.png",
+    "favorite_color": "#585481",
+    'crafting_specialty': "Home cooking",
+    "favorite_food": "Steel Salmon Tempura"
+  },
+  {
+    "name": "Haze",
+    "picture_url": "images/Haze.png",
+    "favorite_color": "#A4508B",
+    'crafting_specialty': "Talisman-making",
+    "favorite_food": "Wildflower Salad"
+  },
+  {
+    "name": "Addam",
+    "picture_url": "images/Addam.png",
+    "favorite_color": "#AE4944",
+    'crafting_specialty': "Replica-artifact-making",
+    "favorite_food": "Energy Stir-Fry"
+  },
+  {
+    "name": "Mythra",
+    "picture_url": "images/Mythra.png",
+    "favorite_color": "#31D88A",
+    'crafting_specialty': '"Creative" cuisine',
+    "favorite_food": "Miracle Parfait"
+  },
+  {
+    "name": "Minoth",
+    "picture_url": "images/Minoth.png",
+    "favorite_color": "#3B1F2B",
+    'crafting_specialty': "Dramaturgy",
+    "favorite_food": "Ruska Dumpling Soup"
+  },
+  {
+    "name": "Hugo",
+    "picture_url": "images/Hugo.png",
+    "favorite_color": "#40476D",
+    'crafting_specialty': "Machine-tinkering",
+    "favorite_food": "Rhogul à la Ardainaise"
+  },
+  {
+    "name": "Brighid",
+    "picture_url": "images/Brighid.png",
+    "favorite_color": "#B17DE6",
+    'crafting_specialty': "Perfumery",
+    "favorite_food": "Gormotti Sashimi Plate"
+  },
+  {
+    "name": "Aegaeon",
+    "picture_url": "images/Aegaeon.png",
+    "favorite_color": "#2E86AB",
+    "crafting_specialty": "Refined cuisine",
+    "favorite_food": "Rich Platinum Bonbon"
+  }
+
+];
